c09baf67026c81b0a0d83d6bb83b7f08dbdfd822
Kunal Kashyap <kashyapkunal142@gmail.com>
2024-11-05 11:51:54 +0530

    fix return type

--- prime.c
+++ prime.c
@@ -12,11 +12,11 @@ int isPrime(int n)
 
     for(int i = 2; i <= root; ++i) {
         if(n % i == 0) {
-            return 1;
+            return 0;
         }
     }
 
-    return 0;
+    return 1;
 }
 
 int main()
